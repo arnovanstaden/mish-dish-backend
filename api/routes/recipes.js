e5453127b7c54e63149489450216d4d8bcb2ab60
Arno van Staden <arnovanstaden@gmail.com>
2020-05-03 11:14:37 +0200

    update cloudinary quality

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -147,7 +147,7 @@ router.post("/", checkAuth, upload.fields([{
                     width: 250,
                     height: 250,
                     crop: "lfill",
-                    quality: 50
+                    quality: 80
                 }]
             },
             function (error, result) {
